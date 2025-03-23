42cf27a65bc9b621a3ad1fea692ae6ceb7c8eba3
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-03-23 10:39:09 +0200

    feat: prepare for new naming scheme

--- src/main/java/com/daromir/boot/info/Constants.java
+++ src/main/java/com/daromir/boot/info/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 IQKV Team, and the original author or authors from the JHipster project.
+ * Copyright 2025 Daromir Team, and the original author or authors from the JHipster project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.iqkv.boot.info;
+package com.daromir.boot.info;
 
 /**
  * App runtime constants.
